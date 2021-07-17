164a0e51de68d86bc24cc414415350d732d9b77e
Rolemodel01291 <kopedaniela0@gmail.com>
2021-07-17 03:53:20 -0700

    origin

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ export default function api() {
       method,
       headers: {
         'Content-Type': 'application/json',
-        
+        'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify(body),
     })
@@ -12,20 +12,20 @@ export default function api() {
 
     return {
       post: async (path, body) => {
-        const response = await fetch(BASE_URL + `api/${path}`, options('POST', body))
+        const response = await fetch(BASE_URL + `api/${path}`, options('POST', body), { mode: 'no-cors' })
         return await response.json()
       },
       get: async (path) => {
-        const response = await fetch(BASE_URL + `api/${path}`, options('GET'))
+        const response = await fetch(BASE_URL + `api/${path}`, options('GET'), { mode: 'no-cors' })
         return await response.json()
       },
   
       patch: async (path, body) => {
-        const response = await fetch(BASE_URL + `api/${path}`, options('PATCH', body))
+        const response = await fetch(BASE_URL + `api/${path}`, options('PATCH', body), { mode: 'no-cors' })
         return await response.json()
       },
       delete: async (path) => {
-        const response = await fetch(BASE_URL + `api/${path}`, options('DELETE'))
+        const response = await fetch(BASE_URL + `api/${path}`, options('DELETE'), { mode: 'no-cors' })
         return await response.json()
       },
     }
